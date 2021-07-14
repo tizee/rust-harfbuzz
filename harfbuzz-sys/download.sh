b253ebbfd353d2269173dba4ec4219e364523dcc
tizee <33030965+tizee@users.noreply.github.com>
2021-07-14 13:46:23 +0800

    wip: freetype integration

---
+++ harfbuzz-sys/download.sh
@@ -0,0 +1,5 @@
+wget https://github.com/harfbuzz/harfbuzz/releases/download/2.6.8/harfbuzz-2.6.8.tar.xz
+rm -rf harfbuzz
+tar xvf harfbuzz-2.6.8.tar.xz
+rm harfbuzz-2.6.8.tar.xz
+mv harfbuzz-2.6.8 harfbuzz
--- harfbuzz-sys/wrapper.h
+++ harfbuzz-sys/wrapper.h
@@ -1,3 +1,4 @@
 #include "harfbuzz/src/hb.h"
+#include "harfbuzz/src/hb-ft.h"
 #include "harfbuzz/src/hb-ot.h"
 #include "harfbuzz/src/hb-aat.h"
